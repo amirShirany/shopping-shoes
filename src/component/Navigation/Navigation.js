f64e25470d529737c17f195fd59e5dbe12f07b68
Amir.sh <mobilefactory012@gmail.com>
2023-03-06 07:18:17 +0330

    Edit : navigate component

--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -1,46 +1,49 @@
 /** @format */
-import { NavLink } from 'react-router-dom';
-import { useAuth } from '../../Providers/AuthProvider';
-import { useCart } from '../../Providers/CartProvider';
-import './Navigation.css';
+import { NavLink } from "react-router-dom"
+import { useAuth } from "../../Providers/AuthProvider"
+import { useCart } from "../../Providers/CartProvider"
+import "./Navigation.css"
 
 const Navigation = () => {
-	const { cart } = useCart();
-	const userData = useAuth();
-	return (
-		<header className='mainNavigation'>
-			<nav>
-				<ul>
-					<div>"Amirhossein Shoping"</div>
-					<li>
-						<NavLink
-							to='/'
-							className={(navData) => (navData.isActive ? 'activeLink' : '')}>
-							home
-						</NavLink>
-					</li>
-				</ul>
+  const { cart } = useCart()
+  const userData = useAuth()
+  return (
+    <header className="mainNavigation">
+      <nav>
+        <ul>
+          <div>"Amirhossein Shoping"</div>
+          <li>
+            <NavLink
+              to="/"
+              className={(navData) => (navData.isActive ? "activeLink" : "")}
+            >
+              home
+            </NavLink>
+          </li>
+        </ul>
 
-				<ul>
-					<li className='cartLink'>
-						<NavLink
-							to='/cart'
-							className={(navData) => (navData.isActive ? 'activeLink' : '')}>
-							cart
-						</NavLink>
-						<span>{cart.length}</span>
-					</li>
-					<li>
-						<NavLink
-							to={userData ? '/profile' : '/login'}
-							className={(navData) => (navData.isActive ? 'activeLink' : '')}>
-							{userData ? 'profile' : 'login/signup'}
-						</NavLink>
-					</li>
-				</ul>
-			</nav>
-		</header>
-	);
-};
+        <ul>
+          <li className="cartLink">
+            <NavLink
+              to="/cart"
+              className={(navData) => (navData.isActive ? "activeLink" : "")}
+            >
+              cart
+            </NavLink>
+            <span>{cart.length}</span>
+          </li>
+          <li>
+            <NavLink
+              to={userData ? "/profile" : "/login"}
+              className={(navData) => (navData.isActive ? "activeLink" : "")}
+            >
+              {userData ? "profile" : "login/signup"}
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
-export default Navigation;
+export default Navigation
